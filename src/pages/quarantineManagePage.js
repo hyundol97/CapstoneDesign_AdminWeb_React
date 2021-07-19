63b7d417154a5d015e64b0979570554e7692f47a
hyundol97 <57279811+hyundol97@users.noreply.github.com>
2021-07-19 11:06:38 +0900

    Update quarantineManagePage.js
    1. Delete app Key and Id

--- src/pages/quarantineManagePage.js
+++ src/pages/quarantineManagePage.js
@@ -4,12 +4,12 @@ import { Table, Button, Icon } from 'semantic-ui-react'
 import firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCf6G8TDQC4gML05q9VqajOnHlF9T2x66I",
+  apiKey: "**",
   authDomain: "userapp-c55de.firebaseapp.com",
   projectId: "userapp-c55de",
   storageBucket: "userapp-c55de.appspot.com",
   messagingSenderId: "599352656145",
-  appId: "1:599352656145:web:a7a442a399dd01e726ccb0"
+  appId: "**"
 };
 // Initialize Firebase
 if (!firebase.apps.length) {
